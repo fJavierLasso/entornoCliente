085975a4bd58dc535559c962395584ac1ab0ee02
Javier Lasso <lassavins@gmail.com>
2022-10-07 21:35:27 +0200

    hecho hasta el 3 de entrada

--- 3.ejerciciosEntrada.js
+++ 3.ejerciciosEntrada.js
@@ -19,12 +19,42 @@ function ejercicio1(){
 // Ejercicio 2
 
 const radio2 = document.getElementById("intRadio");
-
+const resultados2= document.getElementById("resultados2");
 function ejercicio2(){
 
     if (radio2.value) {
-        resultados2.innerHTML = `El área que buscas es ${radio2.value*2}`;
+        resultados2.innerHTML = `El área que buscas es ${Math.round(Math.PI * Math.pow(radio2.value,2)*100)/100}`;
     } else {
         resultados2.innerHTML = `Qué tal si rellenas los campos antes, crack?`;
     }
 }
+
+//Ejercicio 3
+const resultados3= document.getElementById("resultados3");
+function ejercicio3(){
+    
+    //declaro dentro de la función porque si no, no cogería ningún valor!
+    var edades = document.querySelectorAll(".edadEj3");
+
+    var edadesInt = [];
+    
+    //todos los document.get son HTMLobject. Podemos coger su value y transformarlo al tipo que queramos para trabajarlo.
+    for($=0;$<edades.length;$++){
+        edadesInt.push(parseInt(edades[$].value));
+    }
+
+    //magia entre ints.
+    edadesInt.sort();
+
+    var pregunta1 = edadesInt[0] + edadesInt[edadesInt.length-1];
+    var pregunta2 = edadesInt[1] * edadesInt[2];
+
+    resultados3.innerHTML = `El mayor y el menor suman ${pregunta1}. Los dos del medio multiplicados hacen ${pregunta2}.`;
+    
+}
+
+
+
+
+
+
